89bb7f645e25d9e659cade964dfe96d84db5be4c
Dima <dimapanteleyvrach@gmail.com>
2023-06-05 22:02:58 +0300

    дз

---
+++ src/main/java/com/homework/ShopController.java
@@ -0,0 +1,25 @@
+package com.homework;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/store/order/")
+public class ShopController {
+    private final ShopService shopService;
+    public ShopController(ShopService shopService) {
+        this.shopService = shopService;
+    }
+    @GetMapping("/add")
+    public String add(@RequestParam ("name") String name, @RequestParam("price") Double price){
+        shopService.add(name, price);
+        return "товар <b> " + name + " " + price + "</b> добавлен";
+    }
+
+    @GetMapping("/get")
+    public String get(){
+        return shopService.get();
+    }
+}
